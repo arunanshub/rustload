d6a202dd1f37a318462d0a9c6b55e5708f27a19f
Arunanshu Biswas <mydellpc07@gmail.com>
2021-08-11 00:36:57 +0530

    feat(main): add base for signal handlers
    There are some code that are just for experimentation purposes, so deal with it!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
-use std::{env::temp_dir, path::PathBuf};
+use std::{
+    env::temp_dir,
+    path::PathBuf,
+    process::exit,
+    thread::{self, sleep},
+    time::Duration,
+};
 
 use anyhow::{Context, Result};
 use daemonize::Daemonize;
 use lazy_static::lazy_static;
+use signal_hook::{
+    consts::{SIGHUP, SIGINT, SIGQUIT, SIGTERM, SIGUSR1, SIGUSR2},
+    iterator::Signals,
+    low_level::signal_name,
+};
 
 mod cli;
 mod config;
@@ -34,6 +45,48 @@ fn daemonize() -> Result<()> {
     Ok(())
 }
 
+/// Install signal handlers and spawn a thread to handle them.
+///
+/// TODO: add signal handlers:
+/// 1. If SIGTERM is received, shut down the daemon and exit cleanly.
+/// 2. If SIGHUP is received, reload the configuration files, if this
+///    applies.
+fn handle_signals() -> Result<()> {
+    let mut signals =
+        Signals::new(&[SIGINT, SIGQUIT, SIGTERM, SIGHUP, SIGUSR1, SIGUSR2])
+            .log_on_err("Failed to install signal handler")
+            .with_context(|| "Failed to install signal handler")?;
+
+    log::info!("Installed signal handler.");
+
+    thread::spawn(move || {
+        // TODO: consider while let
+        for sig in signals.forever() {
+            match sig {
+                SIGHUP => {
+                    // TODO: Reload conf and log
+                }
+                SIGUSR1 => {
+                    // TODO: dump statelog and conflog
+                }
+                SIGUSR2 => {
+                    // TODO: save statefile and exit
+                }
+                // default case: exit
+                _ => {
+                    log::info!(
+                        "Caught: {:?} (as integer: {:?}). Exit requested.",
+                        signal_name(sig).unwrap_or("UNKNOWN"),
+                        sig,
+                    );
+                    exit(sig);
+                }
+            }
+        }
+    });
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let opt = cli::Opt::from_args();
     crate::logging::enable_logging(&opt)?;
@@ -43,13 +96,15 @@ fn main() -> Result<()> {
         .log_on_err(format!("Cannot open {:?}", opt.conffile))?;
     log::info!("Configuration = {:#?}", cfg);
 
-    // TODO: add signal handlers:
-    // 1. If SIGTERM is received, shut down the daemon and exit cleanly.
-    // 2. If SIGHUP is received, reload the configuration files, if this applies.
-
     if !opt.foreground {
         daemonize()?;
     }
+
+    handle_signals()?;
+
+    // test function
+    log::warn!("Sleeping");
+    sleep(Duration::from_secs(10));
 
     // TODO: begin work here and clean up
     Ok(())
