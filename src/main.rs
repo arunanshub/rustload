d0c78fc95796402ea3c9a7fb0f3540ff8b02d13c
Arunanshu Biswas <mydellpc07@gmail.com>
2021-09-18 14:55:16 +0530

    docs(main): add doc for Rustload

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-#![deny(unused_imports)]
+// vim:set et sw=4 ts=4 tw=79:
+//! Rustload is a daemon process that prefetches binary files and shared
+//! libraries from the hard disc to the main memory of the computer system to
+//! achieve faster application startup time. Rustload is adaptive: it monitors
+//! the application that the user runs, and by analyzing this data, predicts
+//! what applications he might run in the near future, and fetches those
+//! binaries and their dependencies into memory.
+//!
+//! It builds a Markov-based probabilistic model capturing the correlation
+//! between every two applications on the system. The model is then used to
+//! infer the probability that each application may be started in the near
+//! future. These probabilities are used to choose files to prefetch into the
+//! main memory. Special care is taken to not degrade system performance and
+//! only prefetch when enough resources are available.
+//!
+//! ## Citation
+//!
+//! Esfahbod, B. (2006). Preload — an adaptive prefetching daemon. Retrieved
+//! September 18, 2021, from
+//! <https://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.138.2940&rep=rep1&type=pdf>.
 
+#![deny(unused_imports)]
 // Allow some checks during development, but warn about them when releasing.
 #![cfg_attr(
     debug_assertions,
