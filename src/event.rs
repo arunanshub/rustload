ee183cc0068e4a21908aa0051df775a9e82dd2e5
Arunanshu Biswas <mydellpc07@gmail.com>
2021-12-27 13:52:47 +0530

    feat(event): add initial implementation for autosave, tick and tick2
    Note that tick and tick2 are not correctly set up.

--- src/event.rs
+++ src/event.rs
@@ -1,7 +1,18 @@
-use calloop::LoopSignal;
-use diesel::SqliteConnection;
+use std::{convert::TryInto, time::Duration};
 
-use crate::{cli, config, state};
+use anyhow::Result;
+use calloop::{timer::Timer, LoopHandle, LoopSignal};
+use diesel::SqliteConnection;
+use log::Level;
+
+use crate::{
+    cli,
+    common::LogResult,
+    config,
+    model::SortStrategy,
+    prophet, spy,
+    state::{self, State},
+};
 
 /// Holds the data that will be shared across our event loop. Notably, it also
 /// contains a [`LoopSignal`] object that will allow us to stop the event loop
@@ -29,5 +40,121 @@ impl SharedData {
             opt,
             conn,
         }
+    }
+}
+
+impl State {
+    /// Autosaves the state file after a fixed period of time. The time is
+    /// governed by the parameter
+    /// [`System::autosave`](crate::model::System::autosave).
+    fn autosave(
+        handle: LoopHandle<SharedData>,
+        shared: &mut SharedData,
+    ) -> Result<()> {
+        let timer = Timer::new()?;
+        let delay_from_now =
+            Duration::from_secs(shared.conf.system.autosave as u64);
+        timer.handle().add_timeout(delay_from_now, ());
+
+        handle.insert_source(timer, move |_, meta, shared| {
+            if shared.state.save(&shared.conn).is_err() {
+                shared.signal.stop()
+            }
+            meta.add_timeout(delay_from_now, ());
+        })?;
+        Ok(())
+    }
+
+    pub(crate) fn run(
+        handle: LoopHandle<SharedData>,
+        shared: &mut SharedData,
+    ) -> Result<()> {
+        // set up ticker
+        Self::autosave(handle.clone(), shared)?;
+        Self::tick(handle.clone(), shared)?;
+        Self::tick2(handle.clone(), shared)?;
+        Ok(())
+    }
+
+    fn tick(
+        handle: LoopHandle<SharedData>,
+        shared: &mut SharedData,
+    ) -> Result<()> {
+        let timer = Timer::new()?;
+        let delay_from_now = Duration::from_secs(0);
+        timer.handle().add_timeout(delay_from_now, ());
+
+        let handle_clone = handle.clone();
+        handle.insert_source(timer, |_, meta, shared| {
+            let conf = &shared.conf;
+            let state = &mut shared.state;
+
+            if conf.system.doscan {
+                log::debug!("State scanning begin");
+                spy::scan(state, Some(&conf.system.mapprefix))
+                    .log_on_err(Level::Warn, "Failed to scan")
+                    .ok();
+                state.dump_log();
+                state.dirty = true;
+                state.model_dirty = true;
+                log::debug!("State scanning end")
+            }
+            if conf.system.dopredict {
+                prophet::predict(
+                    state,
+                    conf.model.usecorrelation,
+                    shared
+                        .conf
+                        .system
+                        .sortstrategy
+                        .try_into()
+                        .unwrap_or(SortStrategy::Block),
+                    conf.model.memtotal,
+                    conf.model.memfree,
+                    conf.model.memcached,
+                )
+                .log_on_err(Level::Warn, "Failed to predict")
+                .ok();
+            }
+
+            state.time += conf.model.cycle as i32 / 2;
+            meta.add_timeout(
+                Duration::from_secs((conf.model.cycle as u64 + 1) / 2),
+                (),
+            );
+        })?;
+        Ok(())
+    }
+
+    fn tick2(
+        handle: LoopHandle<SharedData>,
+        shared: &mut SharedData,
+    ) -> Result<()> {
+        let timer = Timer::new()?;
+        let delay_from_now = Duration::from_secs(0);
+        timer.handle().add_timeout(delay_from_now, ());
+
+        handle.insert_source(timer, |_, meta, shared| {
+            let conf = &shared.conf;
+            let state = &mut shared.state;
+
+            if state.model_dirty {
+                spy::update_model(
+                    state,
+                    &conf.system.mapprefix,
+                    conf.model.minsize as u64,
+                    conf.model.cycle,
+                )
+                .log_on_err(Level::Warn, "Failed to update model")
+                .ok();
+            }
+
+            state.time += conf.model.cycle as i32 / 2;
+            meta.add_timeout(
+                Duration::from_secs(conf.model.cycle as u64 / 2),
+                (),
+            );
+        })?;
+        Ok(())
     }
 }
